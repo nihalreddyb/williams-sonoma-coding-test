dee45bc922f42a10cbaec1bae6e64a837834a821
Nihal <nihalreddy18@gmail.com>
2021-06-03 00:23:29 -0400

    Base Code Setup for Inital Commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="root-component">
+            William sonoma coding challenge.
+        </div>
+    );
+}
+
+export default App;
